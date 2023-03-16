3e90816a6596e9746fe31d0f97d3fa4b4d441321
lucia <74472796+luciamvm@users.noreply.github.com>
2023-03-16 15:55:38 +0000

    init cylinders rotating

---
+++ components/cylinder.tsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react';
+import { ThreeElements, useFrame, useLoader } from 'react-three-fiber';
+import { Mesh, TextureLoader } from 'three';
+
+const Cylinders = (props: ThreeElements['mesh']) => {
+  const leftCylinderRef = useRef<Mesh>(null);
+  const rightCylinderRef = useRef<Mesh>(null);
+
+  const firstTexture = useLoader(TextureLoader, 'digital.png');
+  const secondTexture = useLoader(TextureLoader, 'digital.png');
+
+  useFrame(() => {
+    leftCylinderRef.current!.rotation.x += 0.01;
+    rightCylinderRef.current!.rotation.x -= 0.01;
+  });
+
+  return (
+    <>
+      <mesh
+        {...props}
+        ref={leftCylinderRef}
+        rotation={[0, 0, Math.PI / 2]}
+        position={[-0.75, 0, 0]}
+      >
+        <cylinderGeometry args={[1.3, 1.3, 1.3, 30]} />
+
+        <meshStandardMaterial color="lightGray" map={firstTexture} />
+      </mesh>
+
+      <mesh
+        {...props}
+        ref={rightCylinderRef}
+        rotation={[0, 0, Math.PI / 2]}
+        position={[0.75, 0, 0]}
+      >
+        <cylinderGeometry args={[1.3, 1.3, 1.3, 30]} />
+
+        <meshStandardMaterial color="lightGray" map={secondTexture} />
+      </mesh>
+    </>
+  );
+};
+
+export default Cylinders;
